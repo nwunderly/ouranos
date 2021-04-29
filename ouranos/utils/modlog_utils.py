8dfc5fec49f6d06bd8daf10c4d3a0abe2816558f
nwunderly <48489521+nwunderly@users.noreply.github.com>
2021-04-28 22:18:45 -0400

    fix dumb error

--- ouranos/utils/modlog_utils.py
+++ ouranos/utils/modlog_utils.py
@@ -117,7 +117,7 @@ def format_edited_log_message(content, **kwargs):
         entry[match.group(1).lower()] = match.group(2)
 
     entry.update(kwargs)
-    return first_line + "".join(f"**{key.capitalize()}:** {entry[key]}\n" for key in order)
+    return first_line + "".join(f"**{key.capitalize()}:** {entry[key]}\n" for key in order if key in entry)
 
 
 def format_small_log_message(emoji, title, user, infraction_id):
