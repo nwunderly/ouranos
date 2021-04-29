fcb13267b8469c06f0cc8b90121c52053db6b190
nwunderly <48489521+nwunderly@users.noreply.github.com>
2021-04-29 13:41:47 -0400

    bump version

--- ouranos/settings.py
+++ ouranos/settings.py
@@ -2,7 +2,7 @@ import discord
 
 
 class Settings:
-    version = '1.1.7'
+    version = '1.1.8'
     prefix = "?"
     description = 'A simple and opinionated Discord moderation bot.'
     author = "nwunder#4003"
