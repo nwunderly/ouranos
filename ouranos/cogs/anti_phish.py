f338955f734555ca6545525691214d3ac7d9f28f
nwunderly <48489521+nwunderly@users.noreply.github.com>
2021-10-30 17:45:09 -0400

    add bitly redirect support to anti_phish
    easily expandable redirect following, only follows links whose domain is specified (security reasons, should be enough for the time being)
    
    will maybe make proxy worker to run requests through if requesting every url ends up being required.

--- ouranos/cogs/anti_phish.py
+++ ouranos/cogs/anti_phish.py
@@ -15,6 +15,11 @@ from ouranos.utils.modlog import LogEvent
 from ouranos.utils.errors import OuranosCommandError, BotMissingPermission, BotRoleHierarchyError, ModActionOnMod
 
 
+FOLLOW_REDIRECT_DOMAINS = ('bit.ly',)
+# copilot recommendations (TODO: maybe add these later)
+#'goo.gl', 't.co', 'tinyurl.com', 'is.gd', 'ow.ly', 'bitly.com')
+
+
 class AntiPhish(Cog):
     URL_PATTERN = re.compile(
         # r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
@@ -25,16 +30,32 @@ class AntiPhish(Cog):
         self.bot = bot
         self.session = aiohttp.ClientSession()
 
+    def cog_unload(self):
+        self.bot.loop.create_task(self.cleanup())
+
     async def cleanup(self):
         await self.session.close()
 
     def get_domains(self, content):
-        content = content.replace("\u0000", "")  # NUL char handling (temp fix)
+        content = content.replace("\u0000", "")  # NUL char handling (temp fix) (TODO)
         urls = [match.group(0) for match in self.URL_PATTERN.finditer(content)]
-        domains = set(urlparse(url).netloc for url in urls)
+
+        # domains = set(urlparse(url).netloc for url in urls)
+        # bitly link handling (temp fix) (TODO)
+        domains = set()
+        to_follow = set()
+        for url in urls:
+            parsed = urlparse(url)
+            if parsed.netloc:
+                if parsed.netloc.startswith(FOLLOW_REDIRECT_DOMAINS):
+                    to_follow.add(url)
+                else:
+                    domains.add(parsed.netloc)
+
         if "" in domains:
             domains.remove("")
-        return domains
+
+        return domains, to_follow
 
     async def is_phish_domain(self, domain):
         async with self.session.get(f"{PHISH_API}/check/{domain}",
@@ -42,7 +63,12 @@ class AntiPhish(Cog):
             resp.raise_for_status()
             return await resp.json()
 
-    async def _do_auto_ban(self, guild, user, message, domain):
+    async def follow_redirect(self, url):
+        async with self.session.get(url, allow_redirects=False) as resp:
+            if 300 <= resp.status < 400:
+                return resp.headers['Location']
+
+    async def _do_auto_ban(self, guild, user, message, domain, from_redirect):
         """Automatically bans a user and dispatches the event to the modlog."""
         mod = guild.me
         duration = None
@@ -59,31 +85,71 @@ class AntiPhish(Cog):
 
         # ban the user (and delete messages from that user)
         audit_reason = f"anti_phish: Phishing link detected ({domain})"
+        if from_redirect:
+            audit_reason += f" (from {from_redirect})"
         await guild.ban(user, reason=audit_reason, delete_message_days=1)
 
         # dispatch the modlog event
-        reason = f"Phishing link detected ({domain})"
+        reason_domain = f"{from_redirect} -> {domain}" if from_redirect else domain
+        reason = f"Phishing link detected ({reason_domain})"
         await LogEvent('autoban', guild, user, mod, reason, None, duration).dispatch()
 
-    async def process_phishing(self, message):
+    async def process_phishing(self, content):
         # runs regex to find URLs and uses urlparse to extract domain for each
-        domains = self.get_domains(message.content)
+        domains, to_follow = self.get_domains(content)
 
-        if not domains or await is_server_mod(message.author):
-            return
+        if not (domains or to_follow):
+            return None, None
 
+        # follow redirects and add those domains to the list
+        # note: this implementation only follows redirects one level deep (intentional)
+        domains_from_redirect = {}
+        for url in to_follow:
+            new_url = await self.follow_redirect(url)
+            if new_url:
+                new_domain = urlparse(new_url).netloc
+                if new_domain:
+                    if new_domain not in domains_from_redirect:
+                        old_url_parsed = urlparse(url)
+                        domains_from_redirect[new_domain] = old_url_parsed.netloc + old_url_parsed.path
+                    domains.add(new_domain)
+
+        # check API and ban if phishing
         for domain in domains:
             if await self.is_phish_domain(domain):
-                try:
-                    return await self._do_auto_ban(message.guild, message.author, message, domain)
-                except OuranosCommandError:
-                    return
+                from_redirect = domains_from_redirect.get(domain)
+                return domain, from_redirect
+
+        return None, None
 
     @commands.Cog.listener()
     async def on_message(self, message):
         config = await db.get_config(message.guild)
-        if config and config.anti_phish:
-            await self.process_phishing(message)
+        if not (config and config.anti_phish):
+            return
+
+        # ignore server moderators
+        if await is_server_mod(message.author):
+            return
+
+        domain, from_redirect = await self.process_phishing(message.content)
+
+        if domain:
+            try:
+                return await self._do_auto_ban(message.guild, message.author, message, domain, from_redirect)
+            except OuranosCommandError:
+                return
+
+    @command()
+    @server_mod()
+    async def test_antiphish(self, ctx, *, content):
+        """Test anti-phish system."""
+        domain, from_redirect = await self.process_phishing(content)
+
+        if domain:
+            await ctx.send(f"{domain} is a phishing domain" + (f" (from {from_redirect})" if from_redirect else ""))
+        else:
+            await ctx.send("No phishing domains detected.")
 
 
 def setup(bot):
--- ouranos/settings.py
+++ ouranos/settings.py
@@ -2,7 +2,7 @@ import discord
 
 
 class Settings:
-    version = '1.6.1'
+    version = '1.6.2'
     prefix = "?"
     description = 'A simple and opinionated Discord moderation bot.'
     author = "nwunder#4003"
